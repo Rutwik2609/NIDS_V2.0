dab42ee94a4d6f20b5b401321d4e2c96119dad6d
Rutwik2609 <98237672+Rutwik2609@users.noreply.github.com>
2025-05-03 15:10:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import { connectDB } from "./db/db.js";
 import userRoutes from "./routes/user.route.js";
@@ -11,6 +12,8 @@ import resulttRoutes from "./routes/result.route.js";
 
 const app = express();
 const PORT = 5000;
+
+const __dirname =path.resolve();
 
 dotenv.config();
 
@@ -40,7 +43,12 @@ app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/predict", predictRoutes);
 app.use("/api/v1/result", resulttRoutes);
 
-
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
   connectDB();
